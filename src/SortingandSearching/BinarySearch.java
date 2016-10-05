e53f46614ec70041a4968485a49f1ea6fcf7c8fe
Utku Yurter <uyurter@na.edu>
2016-10-05 13:46:11 -0500

    Update BinarySearch.java

--- src/SortingandSearching/BinarySearch.java
+++ src/SortingandSearching/BinarySearch.java
@@ -4,11 +4,29 @@
  * and open the template in the editor.
  */
 package SortingandSearching;
-
 /**
  *
  * @author zulfikar
  */
 public class BinarySearch {
+    
+    public bool binarySearch([T] arr , int min , int max , T lookFor){
+        found = false;
+        int mid = (min + max) / 2;
+        while(!found){
+            if(arr[mid] == lookFor)
+                found = true;
+            else if(arr[mid] > lookFor){
+                min = mid + 1;
+                mid = (min + max) / 2;
+            }
+            else{
+                max = mid - 1;
+                mid = (min + max) / 2;
+            }
+        }
+        return found;
+    }
+    
     
 }
