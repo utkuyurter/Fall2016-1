9c79d4350661db80983a5a5461374a48999fea85
Utku Yurter <uyurter@na.edu>
2016-10-05 14:17:05 -0500

    Update SelectionSort.java

--- src/SortingandSearching/SelectionSort.java
+++ src/SortingandSearching/SelectionSort.java
@@ -10,5 +10,19 @@ package SortingandSearching;
  * @author zulfikar
  */
 public class SelectionSort {
-    
+    public void selectionSort(T[] arr){
+        int min ;
+        T temp;
+        
+        for(int index = 0; index < arr.length() - 1  ; index++){
+            min = index;
+            for(int scan = index+1 ; scan < arr.length; scan++){
+                if(arr[scan].compareTo(arr[min]) < 0)
+                    min = scan;
+                temp = arr[min];
+                arr[min] = arr[index];
+                arr[index] = temp;
+            }            
+        }
+    }
 }
